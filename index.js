98db9e399a8831e201540d45274c91c961aa112a
Cofresi <cofresi@dash.org>
2020-04-12 14:24:39 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+let merk = require('merk')
+
+// create or load store
+let db = merk('./state.db')
+
+// get value
+let value = db.getSync(Buffer.from('mykey'))
+
+// get Merkle root
+let hash = db.rootHash()
+
+// create merkle proof
+let proof = db.proveSync([
+  Buffer.from('key1'),
+  Buffer.from('key2')
+])
+
+// modify values
+db.batch()
+  .put(Buffer.from('key1'), Buffer.from('value1'))
+  .put(Buffer.from('key2'), Buffer.from('value2'))
+  .delete(Buffer.from('key3'))
+  .commitSync()
