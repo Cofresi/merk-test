fdea8568af51d0aa1a097fea2663a6d392057c95
Cofresi <cofresi@dash.org>
2020-04-12 21:34:40 -0400

    refactor

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ let merk = require('merk')
 // create or load store
 let db = merk('./state.db');
 
+let value1;
+let value2;
+
+// get value
+value1 = db.getSync(Buffer.from('key1'));
+value2 = db.getSync(Buffer.from('key2'));
+console.log('key1', value1.toString());
+console.log('key2', value2.toString());
+
 // put value
 db.batch()
   .put(Buffer.from('key1'), Buffer.from('value1'))
@@ -10,8 +19,8 @@ db.batch()
   .commitSync();
 
 // get value
-let value1 = db.getSync(Buffer.from('key1'));
-let value2 = db.getSync(Buffer.from('key2'));
+value1 = db.getSync(Buffer.from('key1'));
+value2 = db.getSync(Buffer.from('key2'));
 console.log('key1', value1.toString());
 console.log('key2', value2.toString());
 
@@ -30,12 +39,16 @@ console.log('proof', proof);
 db.batch()
   .put(Buffer.from('key1'), Buffer.from('value3'))
   .put(Buffer.from('key2'), Buffer.from('value4'))
-  .delete(Buffer.from('key1'))
-  .delete(Buffer.from('key2'))
   .commitSync();
 
 // get value
 value1 = db.getSync(Buffer.from('key1'));
 value2 = db.getSync(Buffer.from('key2'));
-console.log('value key1', value1.toString());
-console.log('value key2', value2.toString());
+console.log('key1', value1.toString());
+console.log('key2', value2.toString());
+
+// delete values
+db.batch()
+  .delete(Buffer.from('key1'))
+  .delete(Buffer.from('key2'))
+  .commitSync();
