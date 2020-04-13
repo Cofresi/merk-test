2ee80c164bc7a760c813fdfe1113117c54722a07
Cofresi <cofresi@dash.org>
2020-04-12 21:21:09 -0400

    add some log output

--- index.js
+++ index.js
@@ -1,23 +1,43 @@
 let merk = require('merk')
 
 // create or load store
-let db = merk('./state.db')
+let db = merk('./state.db');
 
-// get value
-let value = db.getSync(Buffer.from('mykey'))
-
-// get Merkle root
-let hash = db.rootHash()
-
-// create merkle proof
-let proof = db.proveSync([
-  Buffer.from('key1'),
-  Buffer.from('key2')
-])
-
-// modify values
+// put value
 db.batch()
   .put(Buffer.from('key1'), Buffer.from('value1'))
   .put(Buffer.from('key2'), Buffer.from('value2'))
-  .delete(Buffer.from('key3'))
-  .commitSync()
+  .commitSync();
+
+// get value
+let value1 = db.getSync(Buffer.from('key1'));
+let value2 = db.getSync(Buffer.from('key2'));
+console.log('key1', value1.toString());
+console.log('key2', value2.toString());
+
+// get Merkle root
+let hash = db.rootHash();
+console.log('hash', hash);
+
+// create merkle proof
+let proof = db.prove([
+  Buffer.from('key1'),
+  Buffer.from('value1')
+]);
+console.log('proof', proof);
+
+let res = db.verify(proof, [Buffer.from('key1'), Buffer.from('value1')]);
+
+// modify values
+db.batch()
+  .put(Buffer.from('key1'), Buffer.from('value3'))
+  .put(Buffer.from('key2'), Buffer.from('value4'))
+  .delete(Buffer.from('key1'))
+  .delete(Buffer.from('key2'))
+  .commitSync();
+
+// get value
+value1 = db.getSync(Buffer.from('key1'));
+value2 = db.getSync(Buffer.from('key2'));
+console.log('value key1', value1.toString());
+console.log('value key2', value2.toString());
