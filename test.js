afe991753184e1480a0d45bdeee01581fee96b76
Cofresi <cofresi@dash.org>
2020-12-18 18:16:40 -0400

    quorum info

--- test.js
+++ test.js
@@ -27,7 +27,7 @@ function createRpcClient(config = {}) {
 }
 async function init() {
   const client = createRpcClient();
-  const result = await client.quorum('list');
+  const result = await client.quorum('info 1 0000001cf53b5489691d2681e49228a6f2955040c3f0da361b73463d241db452');
   console.log('res', result);
 }
 init();
